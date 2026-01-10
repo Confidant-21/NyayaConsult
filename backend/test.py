2c36367dac9a5add75c15ee63c84b9236fb8a7e8
Confidant-21 <21swastikgupta@gmail.com>
2026-01-10 16:29:03 +0530

    Deleating Trash files used for testing.

--- backend/test.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine, text
-
-engine = create_engine("postgresql+psycopg2://postgres:Confidant_21@localhost:5432/NyayaConsult")
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT 1"))
-    print(result.fetchone())
